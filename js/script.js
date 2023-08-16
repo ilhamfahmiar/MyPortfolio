adbc4d015013099598d90f7e35d820709c870a80
Unkoizumi <ilhamfahmiamrirosada@gmail.com>
2023-08-16 22:01:51 +0700

    New commit

---
+++ js/script.js
@@ -0,0 +1,66 @@
+/* menu icon navbar */
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+/* scroll section active navbar */
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+/* Sticky Selector */
+window.onscroll = () => {
+
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            });
+        };
+    });
+
+    let header = document.querySelector('.header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /* hapus menu navbar ketik click atau scroll */
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+/* change dark mode */
+let change = 0;
+let darkModeIcon = document.querySelector('#darkMode-icon');
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+    if(change == 0){
+        document.getElementById("imgid").src="Images/img-me-dark.png";
+        change = 1;
+    }else{
+        document.getElementById("imgid").src="Images/img-me.png";
+        change = 0;
+    }
+};
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img img, .skills-container, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, about-img img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, home-content p, about-content', { origin: 'right' });
