bd29068205cf6d5fe1c22abf18c2ab324294ec00
FahmiAR <46561696+UnkoIzumi@users.noreply.github.com>
2023-12-21 11:07:29 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -59,14 +59,14 @@ darkModeIcon.onclick = () => {
     }
 };
 
-// ScrollReveal({
-//     reset: true,
-//     distance: '80px',
-//     duration: 2000,
-//     delay: 200
-// });
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
 
-// ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-// ScrollReveal().reveal('.home-img img, .skills-container, .contact form', { origin: 'bottom' });
-// ScrollReveal().reveal('.home-content h1, about-img img', { origin: 'left' });
-// ScrollReveal().reveal('.home-content h3, home-content p, about-content', { origin: 'right' });
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img img, .skills-container, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, about-img img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, home-content p, about-content', { origin: 'right' });
